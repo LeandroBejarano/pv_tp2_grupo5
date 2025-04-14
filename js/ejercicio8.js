9bd590e6c16c924951362a8de3bf910c96feef72
Leandro794 <44125429@fi.unju.edu.ar>
2025-04-14 16:56:37 -0300

    Desarrollo puntos 8-12

---
+++ js/ejercicio8.js
@@ -0,0 +1,13 @@
+function mayor (n1, n2)
+{
+    if (n1 > n2)
+        console.log("El número mayor es " + n1);
+    else if (n1 < n2)
+        console.log("El número mayor es " + n2);
+    else
+        console.log ("Los dos números ingresados son iguales");
+}
+
+mayor (21,12); // caso n1 > n2
+mayor (5,9); // caso n2 > n1
+mayor (11,11); // caso n1 = n2
