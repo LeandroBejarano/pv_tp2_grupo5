c786f4e950b969f189a176b0dc7fa35c1fe4c9a9
Leandro794 <44125429@fi.unju.edu.ar>
2025-04-17 18:57:46 -0300

    Correccion ejercicio 18

--- js/ejercicio18.js
+++ js/ejercicio18.js
@@ -3,3 +3,9 @@ function cambiar()
     let txt = document.getElementById("texto");
     txt.innerText = "Texto cambiado";
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("btnCambiar");
+    btn.addEventListener("click", cambiar);
+});
